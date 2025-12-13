c464d2554a8e4d4ede5ffb0e805dd9963c44d5d4
Subhankar <maitisubhankar55@gmail.com>
2025-12-13 18:53:28 +0530

    commit all to main

---
+++ reactRouter/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import { Outlet } from "react-router-dom";
+
+
+function Layout() {
+
+    return (
+        <>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+        </>
+    );
+}
+
+export default Layout;
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </StrictMode>,
+)
