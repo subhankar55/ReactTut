a13cf906e8fe0e941d31eecd00a36248a2ef55c6
Subhankar <maitisubhankar55@gmail.com>
2025-12-15 20:40:10 +0530

    update all

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -7,7 +7,7 @@ import Card from './components/Card.jsx'
 
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [thememode, setTheme] = useState("light");
   const darkTheme = () => {
     setTheme("dark");
   };
@@ -16,11 +16,11 @@ function App() {
   };
 
   useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(theme);
-  }, [theme]);
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(thememode)
+  }, [thememode]);
   return (
-      <ThemeProvider values={{thememode, darkTheme, lightTheme}}>
+      <ThemeProvider value={{thememode, darkTheme, lightTheme}}>
         <div className="flex flex-wrap min-h-screen items-center">
             <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
