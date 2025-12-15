9947a674ea84a8e910131468a4782dcf9e162c17
Subhankar <maitisubhankar55@gmail.com>
2025-12-15 22:12:51 +0530

    Update Footer.jsx

--- reactRouter/src/components/footer/Footer.jsx
+++ reactRouter/src/components/footer/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
                         <a href="https://github.com/subhankar55/" className="hover:underline">
-                            hiteshchoudhary
+                            subhankarmaiti
                         </a>
                         . All Rights Reserved.
                     </span>
