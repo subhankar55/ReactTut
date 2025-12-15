8e384378f22d6e480235081be5e43bb645d4945f
Subhankar <maitisubhankar55@gmail.com>
2025-12-15 16:55:09 +0530

    commit all to main

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import './App.css'
+import { ThemeProvider } from './context/Theme.js'
+import ThemeBtn from './components/ThemeBtn.jsx'
+import Card from './components/Card.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("light");
+  const darkTheme = () => {
+    setTheme("dark");
+  };
+  const lightTheme = () => {
+    setTheme("light");
+  };
 
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(theme);
+  }, [theme]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <ThemeProvider values={{thememode, darkTheme, lightTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                      <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                      <Card />
+              </div>
+            </div>
+        </div>
+      </ThemeProvider>
   )
 }
 
